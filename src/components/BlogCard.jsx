65de93bdf51968d24732fd2f9a5dc1782e95c2a0
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-25 00:07:36 +0530

    set up blog card and preview component and page

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,7 @@
+const BlogCard = () => {
+    return(
+        <></>
+    )
+}
+
+export default BlogCard
---
+++ src/pages/BlogPreview.jsx
@@ -0,0 +1,7 @@
+const BlogPreview = () => {
+    return(
+        <></>
+    )
+}
+
+export default BlogPreview
