bd80a49822757520ccc32c8707acbd84c06654eb
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-28 22:43:45 +0530

    created a function to validate the type of file uploaded to ensure only a pdf file can be uploaded while filling the form

--- src/components/CareerForm.jsx
+++ src/components/CareerForm.jsx
@@ -7,6 +7,7 @@ const CareerForm = ({careers}) => {
     const {register, handleSubmit, formState: {errors}} = useForm();
     const [successMessage, setSuccessMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    
     const onSubmit = async (data) => {
         console.log('Form Data:', data);   
         const formData = new FormData()
@@ -53,7 +54,7 @@ const CareerForm = ({careers}) => {
             <input {...register('email', {required: 'Please enter your email', pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i})} className="border border-gray-400 w-96 h-8 rounded-md pl-2 mt-4" type="text" placeholder="Email Address *" />
             {errors?.email?.type === 'required' && <p className='text-red-600'>{errors.email.message}</p>}
             {errors?.email?.type === 'pattern' && <p className='text-red-600'>Please enter a valid email</p>}
-            <input {...register('file', {required: 'Please upload your CV'})} type='file' className='mt-4' />
+            <input {...register('file', {required: 'Please upload your CV', validate: validateUploadedFile})} type='file' accept='application/pdf' className='mt-4' />
             <p>*Upload PDF only</p>
             {errors?.file && <p className='text-red-600'>{errors.file.message}</p>}
             <input type="submit" value="Apply" className='w-28 h-8 bg-black text-white transition-colors duration-300 ease-in-out apply-now-button rounded-lg hover:bg-bluetheme cursor-pointer mt-6' />
@@ -65,6 +66,19 @@ const CareerForm = ({careers}) => {
         </div>
         
     )
+}
+
+const validateUploadedFile = (value) => {
+    if(!value || !value[0]){
+        return 'Please upload your CV'
+    }
+
+    const pdfType = ['application/pdf']
+    const fileType = value[0]?.type;
+
+    if(!pdfType.includes(fileType)){
+        return 'Please upload your CV in PDF format'
+    }
 }
 
 export default CareerForm;
