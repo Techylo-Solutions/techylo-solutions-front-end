d227841a63e4e98938ef5c1f08761c6de9d3ee41
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-03-08 22:57:57 +0530

    about us poster

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import Nav from "../components/Nav";
+import AboutPoster from "/assets/about/about-poster.jpg"
 
 const About = () => {
     return(
         <>
             <Nav />
+            <div className="mt-16">
+                <img src={AboutPoster} className="w-1/2 rounded-tl-2xl rounded-br-3xl" />
+            </div>
             
          
         </>
